68df1767e28cd92fca602a0f6baa2506b7250dc5
Jonsson <yz808@outlook.com>
2020-08-05 00:09:11 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios';
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://localhost:8888/card'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 new Vue({
