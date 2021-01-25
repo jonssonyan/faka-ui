92c4fef55122bf4b8b1af2fa52b8ee0a92995259
jonssonyan <yz808@outlook.com>
2021-01-26 00:21:49 +0800

    fix:界面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
     <el-container class="home-container">
-        <!--头部区域-->
-        <el-header>
-            <div>
-                <img src="../assets/logo.png" alt="">
-                <span>以梦商户后台管理面板</span>
-            </div>
-            <el-badge :value="3" class="item">
-                <el-button size="small">未处理订单</el-button>
-            </el-badge>
-            <el-button type="info" @click="logout">退出</el-button>
-        </el-header>
         <!--主题区域-->
         <el-container>
             <!--侧边栏-->
-            <el-aside :width="isCollapse?'64px':'200px'">
-                <div class="toggle-button" @click="toggleCollapse">|||</div>
+            <el-aside :width="isCollapse?'64px':'255px'">
+                <div class="sidebar-logo-container">
+                    <router-link v-if="isCollapse" class="sidebar-logo-link" to="/home">
+                        <img src="../assets/logo.png" class="sidebar-logo">
+                    </router-link>
+                    <router-link v-else class="sidebar-logo-link" to="/home">
+                        <img src="../assets/logo.png" class="sidebar-logo">
+                        <h1 class="sidebar-title">以梦系统</h1>
+                    </router-link>
+                </div>
                 <el-menu
                         background-color="#333744"
                         text-color="#fff"
@@ -47,6 +44,14 @@
             <el-container>
                 <!--右侧主题内容-->
                 <el-main>
+                    <!--头部区域-->
+                    <el-header>
+                        <div class="toggle-button" @click="toggleCollapse">|||</div>
+                        <el-badge :value="3" class="item">
+                            <el-button size="small">未处理订单</el-button>
+                        </el-badge>
+                        <el-button type="info" @click="logout">退出</el-button>
+                    </el-header>
                     <router-view></router-view>
                 </el-main>
             </el-container>
@@ -110,12 +115,42 @@
             display: flex;
             align-items: center;
 
-            span {
-                margin-left: 15px;
+            .avatar-container {
+                margin-right: 10px;
+
+                .avatar-wrapper {
+                    margin-top: 5px;
+                    position: relative;
+
+                    .user-name {
+                        vertical-align: top;
+                        font-size: .9rem;
+                        margin-left: 5px;
+                        margin-top: -4px;
+                        display: inline-block;
+                    }
+
+                    .user-avatar {
+                        cursor: pointer;
+                        width: 2rem;
+                        height: 2rem;
+                        border-radius: 50%;
+                        vertical-align: text-bottom;
+                    }
+
+                    .el-icon-caret-bottom {
+                        cursor: pointer;
+                        position: absolute;
+                        right: -20px;
+                        top: 25px;
+                        font-size: 12px;
+                    }
+                }
             }
         }
     }
 
+    // 左侧导航栏
     .el-aside {
         background-color: #333744;
 
@@ -124,12 +159,9 @@
         }
     }
 
+    // 右侧主区域
     .el-main {
         background-color: #EAEDF1;
-    }
-
-    .iconfont {
-        margin-right: 10px;
     }
 
     .toggle-button {
@@ -140,5 +172,43 @@
         text-align: center;
         letter-spacing: 0.2em;
         cursor: pointer;
+    }
+
+    .sidebar-logo-container {
+        position: relative;
+        width: 100%;
+        height: 54px;
+        line-height: 54px;
+        background: #333744;
+        text-align: center;
+        overflow: hidden;
+
+        .sidebar-logo-link {
+            height: 100%;
+            width: 100%;
+        }
+
+        .sidebar-logo {
+            width: 32px;
+            margin-right: 12px;
+            display: inline-block;
+            vertical-align: middle;
+            margin-bottom: -2px;
+        }
+
+        .sidebar-title {
+            display: inline-block;
+            margin: 0;
+            color: #fff;
+            font-weight: 500;
+            line-height: 50px;
+            font-size: 19px;
+            vertical-align: middle;
+
+            span {
+                font-size: 12px;
+                letter-spacing: 1px;
+            }
+        }
     }
 </style>
