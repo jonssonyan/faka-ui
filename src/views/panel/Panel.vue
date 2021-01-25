c7a78fae2cfb21c26ff5aedde8035a0476780849
jonssonyan <yz808@outlook.com>
2021-01-25 21:57:08 +0800

    fix:图表

--- src/views/panel/Panel.vue
+++ src/views/panel/Panel.vue
@@ -43,103 +43,104 @@
         data() {
             return {
                 orderXAxis: [],
-                orderSeries: []
+                orderSeries: [],
+                priceXAxis: [],
+                priceSeries: [],
             }
         },
-        beforeMount() {
-            this.orderCharts();
-        },
         mounted() {
-            this.drawOrder();
-            this.drawPrice();
+            this.drawChart();
         },
         methods: {
-            async orderCharts() {
-                let _orderXAxis = [];
-                let _orderSeries = [];
-                let {data: res} = await this.$http.post(`/api/order/orderCharts`, {});
-                if (res.code !== 1) {
-                    this.$message.error(res.msg)
-                } else {
-                    let map = res.data;
-                    Object.keys(map).forEach(function (key) {
-                        _orderXAxis.push(key);
-                        _orderSeries.push(map[key]);
-                    });
-                }
-                this.orderXAxis = _orderXAxis;
-                this.orderSeries = _orderSeries;
-                console.log(this.orderXAxis);
-                console.log(this.orderSeries);
-            },
-            drawOrder() {
-                console.log(this.orderXAxis);
-                console.log(this.orderSeries);
-                // 基于准备好的dom，初始化echarts实例
-                const myChart = this.$echarts.init(document.getElementById('order'));
-                // 绘制图表
-                myChart.setOption({
-                    title: {
-                        text: '订单统计'
-                    },
-                    xAxis: {
-                        type: 'category',
-                        boundaryGap: false,
-                        data: this.orderXAxis
-                    },
-                    yAxis: {
-                        type: 'value'
-                    },
-                    series: [{
-                        data: this.orderSeries,
-                        type: 'line',
-                        areaStyle: {}
-                    }]
-                });
-            },
-            drawPrice() {
-                // 基于准备好的dom，初始化echarts实例
-                const myChart = this.$echarts.init(document.getElementById('price'));
-                // 绘制图表
-                myChart.setOption({
-                    title: {
-                        text: '收入统计'
-                    },
-                    color: ['#3398DB'],
-                    tooltip: {
-                        trigger: 'axis',
-                        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-                            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-                        }
-                    },
-                    grid: {
-                        left: '3%',
-                        right: '4%',
-                        bottom: '3%',
-                        containLabel: true
-                    },
-                    xAxis: [
-                        {
+            drawChart() {
+                const that = this;
+                this.$http.post(`/api/order/orderCharts`, {}).then(data => {
+                    const res = data.data;
+                    if (res.code !== 1) {
+                        this.$message.error(res.msg)
+                    } else {
+                        let map = res.data;
+                        Object.keys(map).forEach(function (key) {
+                            that.orderXAxis.push(key);
+                            that.orderSeries.push(map[key]);
+                        });
+                    }
+                    // 基于准备好的dom，初始化echarts实例
+                    const orderChart = this.$echarts.init(document.getElementById('order'));
+                    // 绘制图表
+                    orderChart.setOption({
+                        title: {
+                            text: '订单统计'
+                        },
+                        xAxis: {
                             type: 'category',
-                            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                            axisTick: {
-                                alignWithLabel: true
-                            }
-                        }
-                    ],
-                    yAxis: [
-                        {
+                            boundaryGap: false,
+                            data: that.orderXAxis
+                        },
+                        yAxis: {
                             type: 'value'
-                        }
-                    ],
-                    series: [
-                        {
-                            name: '收入(元)',
-                            type: 'bar',
-                            barWidth: '60%',
-                            data: [10, 52, 200, 334, 390, 330, 1000]
-                        }
-                    ]
+                        },
+                        series: [{
+                            data: that.orderSeries,
+                            type: 'line',
+                            areaStyle: {}
+                        }]
+                    });
+                });
+                this.$http.post(`/api/order/priceCharts`, {}).then(data => {
+                    let res = data.data;
+                    if (res.code !== 1) {
+                        this.$message.error(res.msg)
+                    } else {
+                        let map = res.data;
+                        Object.keys(map).forEach(function (key) {
+                            that.priceXAxis.push(key);
+                            that.priceSeries.push(map[key]);
+                        });
+                    }
+                    // 基于准备好的dom，初始化echarts实例
+                    const priceChart = this.$echarts.init(document.getElementById('price'));
+                    // 绘制图表
+                    priceChart.setOption({
+                        title: {
+                            text: '收入统计'
+                        },
+                        color: ['#3398DB'],
+                        tooltip: {
+                            trigger: 'axis',
+                            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                            }
+                        },
+                        grid: {
+                            left: '3%',
+                            right: '4%',
+                            bottom: '3%',
+                            containLabel: true
+                        },
+                        xAxis: [
+                            {
+                                type: 'category',
+                                data: that.priceXAxis,
+                                axisTick: {
+                                    alignWithLabel: true
+                                }
+                            }
+                        ],
+                        yAxis: [
+                            {
+                                type: 'value'
+                            }
+                        ],
+                        series: [
+                            {
+                                name: '收入(元)',
+                                type: 'bar',
+                                barWidth: '60%',
+                                data: that.priceSeries
+                            }
+                        ]
+                    });
                 });
             }
         }
